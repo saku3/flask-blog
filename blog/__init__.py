8be165778e02c9c87f45f77f02e7476477ef4e41
sakurai <39reyskhky@gmail.com>
2020-04-03 00:36:43 +0900

    errorpage追加

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,8 @@
 from blog.core.views import core
+from blog.error_pages.handlers import error_pages
 from flask import Flask
 
 app = Flask(__name__)
 
-
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
