66911680c9d7f4ec43aefb0caf20d5f77175d10c
sakurai <39reyskhky@gmail.com>
2020-04-09 23:26:52 +0900

    models追加

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,21 @@
+import os
+
 from blog.core.views import core
 from blog.error_pages.handlers import error_pages
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
